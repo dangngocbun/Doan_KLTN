79a080c294d0f6e303155b59b38579a1628a5ed8
dangngocbun <93512493+dangngocbun@users.noreply.github.com>
2023-12-05 10:56:01 +0700

    Create laravel (1).sql

---
+++ database/laravel (1).sql
@@ -0,0 +1,107 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Máy chủ: 127.0.0.1
+-- Thời gian đã tạo: Th12 05, 2023 lúc 04:55 AM
+-- Phiên bản máy phục vụ: 10.4.32-MariaDB
+-- Phiên bản PHP: 8.2.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Cơ sở dữ liệu: `laravel`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Cấu trúc bảng cho bảng `categories`
+--
+-- Error reading structure for table laravel.categories: #1932 - Table &#039;laravel.categories&#039; doesn&#039;t exist in engine
+-- Error reading data for table laravel.categories: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `laravel`.`categories`&#039; at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Cấu trúc bảng cho bảng `failed_jobs`
+--
+-- Error reading structure for table laravel.failed_jobs: #1932 - Table &#039;laravel.failed_jobs&#039; doesn&#039;t exist in engine
+-- Error reading data for table laravel.failed_jobs: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `laravel`.`failed_jobs`&#039; at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Cấu trúc bảng cho bảng `migrations`
+--
+-- Error reading structure for table laravel.migrations: #1932 - Table &#039;laravel.migrations&#039; doesn&#039;t exist in engine
+-- Error reading data for table laravel.migrations: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `laravel`.`migrations`&#039; at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Cấu trúc bảng cho bảng `orders`
+--
+-- Error reading structure for table laravel.orders: #1932 - Table &#039;laravel.orders&#039; doesn&#039;t exist in engine
+-- Error reading data for table laravel.orders: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `laravel`.`orders`&#039; at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Cấu trúc bảng cho bảng `order_items`
+--
+-- Error reading structure for table laravel.order_items: #1932 - Table &#039;laravel.order_items&#039; doesn&#039;t exist in engine
+-- Error reading data for table laravel.order_items: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `laravel`.`order_items`&#039; at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Cấu trúc bảng cho bảng `password_resets`
+--
+-- Error reading structure for table laravel.password_resets: #1932 - Table &#039;laravel.password_resets&#039; doesn&#039;t exist in engine
+-- Error reading data for table laravel.password_resets: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `laravel`.`password_resets`&#039; at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Cấu trúc bảng cho bảng `password_reset_tokens`
+--
+-- Error reading structure for table laravel.password_reset_tokens: #1932 - Table &#039;laravel.password_reset_tokens&#039; doesn&#039;t exist in engine
+-- Error reading data for table laravel.password_reset_tokens: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `laravel`.`password_reset_tokens`&#039; at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Cấu trúc bảng cho bảng `personal_access_tokens`
+--
+-- Error reading structure for table laravel.personal_access_tokens: #1932 - Table &#039;laravel.personal_access_tokens&#039; doesn&#039;t exist in engine
+-- Error reading data for table laravel.personal_access_tokens: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `laravel`.`personal_access_tokens`&#039; at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Cấu trúc bảng cho bảng `products`
+--
+-- Error reading structure for table laravel.products: #1932 - Table &#039;laravel.products&#039; doesn&#039;t exist in engine
+-- Error reading data for table laravel.products: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `laravel`.`products`&#039; at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Cấu trúc bảng cho bảng `users`
+--
+-- Error reading structure for table laravel.users: #1932 - Table &#039;laravel.users&#039; doesn&#039;t exist in engine
+-- Error reading data for table laravel.users: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `laravel`.`users`&#039; at line 1
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
